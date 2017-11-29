0bcfc34081d846f2d3d7b3fbc226057535abd6e5
Johannes Bachhuber <jobachhu@mit.edu>
2017-11-29 13:57:51 +0100

    [COLAB-2322] Remove old typekit from blog

--- header.php
+++ header.php
@@ -17,8 +17,6 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <?php wp_head();?>
-    <script src="https://use.typekit.net/bmu2xym.js"></script>
-    <script>try{Typekit.load({ async: true });}catch(e){}</script>
 </head>
 <body>
     <header>
