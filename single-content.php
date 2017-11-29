2fb353fb44177e1bfe118f1046355c811401744e
Johannes Bachhuber <jobachhu@mit.edu>
2017-11-29 14:37:28 +0100

    [COLAB-2443] Load addthis via https

--- single-content.php
+++ single-content.php
@@ -6,9 +6,9 @@
     </div>
     <div class="post-share clearfix">
         <!--TODO COLAB-2443: remove addthis-->
-        <a href="http://www.addthis.com/bookmark.php?v=250&amp;username=climatecolab" class="addthis_button_compact" addthis:url="<?php the_permalink(); ?>" addthis:title="<?php the_title(); ?>" addthis:description=""> <img src="<?php bloginfo('template_directory');?>/images/icon_share.png" height="23" alt="Share"> </a>
+        <a href="https://www.addthis.com/bookmark.php?v=250&amp;username=climatecolab" class="addthis_button_compact" addthis:url="<?php the_permalink(); ?>" addthis:title="<?php the_title(); ?>" addthis:description=""> <img src="<?php bloginfo('template_directory');?>/images/icon_share.png" height="23" alt="Share"> </a>
         <script type="text/javascript">var addthis_config={data_track_clickback:true};</script>
-        <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=climatecolab"></script>
+        <script type="text/javascript" src="https://s7.addthis.com/js/250/addthis_widget.js#username=climatecolab"></script>
     </div>
     <?php the_content(); ?>
     <div class="post-author">
